0ef26f9a8dee1de4ce7402a1b77d9c3760c2889a
Taylor Faragher <faragher6@gmail.com>
2024-05-21 16:25:48 -0500

    Adding some basic code for connection

--- handlers/lambdas/createLink.ts
+++ handlers/lambdas/createLink.ts
@@ -1,18 +1,59 @@
-import {v4 as uuidv4} from 'uuid';
+import {connect} from 'ts-postgres';
 import {LinkShorteningResponse} from '../../lib/types/types';
+import {getRandomValue} from '../utils/getRandomValue';
+import {SecretsManager} from '@aws-sdk/client-secrets-manager';
+
+const secrets = new SecretsManager({});
 
 export const handler = async (event): Promise<LinkShorteningResponse> => {
     if (!event.body) {
         return {statusCode: 400, body: 'invalid request, you are missing the parameter body'};
     }
+    const {password, username, host, dbname} = await getSecretValue('db-master-user-secret');
+
     const item = typeof event.body == 'object' ? event.body : JSON.parse(event.body);
-    const linkId = uuidv4();
+    const linkId = getRandomValue();
+    console.log('generated linkid: ', linkId);
+    console.log('host: ', host);
+    console.log('dbname: ', dbname);
+    console.log('username: ', username);
+    console.log('password: ', password);
+
+    await using client = await connect({
+        user: username,
+        host: host,
+        database: dbname,
+        password: password,
+    });
+    console.log('connected successfully');
+
+    const query =
+        'CREATE TABLE IF NOT EXISTS Links(id SERIAL PRIMARY KEY,url VARCHAR(2048) NOT NULL,description TEXT,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)';
 
     try {
-        //POST code
-        return {statusCode: 200, body: `Created id ${linkId} for item: ${JSON.stringify(item.url)}`};
+        const result = await client.query(query);
+        return {
+            statusCode: 200,
+            body: `Created id ${linkId} for item: ${JSON.stringify(item.url)}. Also here are the results: ${result}`,
+        };
     } catch (dbError) {
         const errorResponse = 'database error';
         return {statusCode: 500, body: errorResponse};
     }
+};
+
+// const getSecretValue = secretId => {
+//     return new Promise((resolve, reject) => {
+//         secrets.getSecretValue({SecretId: secretId}, (err, data) => {
+//             if (err) return reject(err);
+//             if (data.SecretString) {
+//                 return resolve(JSON.parse(data.SecretString));
+//             }
+//         });
+//     });
+// };
+
+const getSecretValue = async secret => {
+    const data = await secrets.getSecretValue({SecretId: secret});
+    return JSON.parse(data.SecretString as string);
 };
