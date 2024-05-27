cc67e17438188991c905c35ef25d3f5a54a2b579
Taylor Faragher <faragher6@gmail.com>
2024-05-26 23:21:27 -0500

    Adds logic to retrieve all links for user

--- handlers/linkLambdas/retrieveAllUserLinks.ts
+++ handlers/linkLambdas/retrieveAllUserLinks.ts
@@ -1,13 +1,36 @@
+import {connect} from 'ts-postgres';
 import {LinkShorteningResponse} from '../../lib/types/types';
+import {getSecretValue} from '../utils/getSecretValue';
 
 export const handler = async (event): Promise<LinkShorteningResponse> => {
-    if (!event.body) {
+    if (!event.body || event.pathParameters.userId) {
         return {statusCode: 400, body: 'invalid request, you are missing the parameter body'};
     }
+    const userId = event.pathParameters.userId;
+
+    const {password, username, host, dbname} = await getSecretValue('LinkShortenerMasterSecret');
+
+    await using client = await connect({
+        user: username,
+        host: host,
+        database: dbname,
+        password: password,
+    });
+
+    const query = `
+        SELECT l.* FROM links l
+        JOIN linkuseridtolinksid lt ON l.link_id = lt.link_id
+        WHERE lt.user_id = $1;
+    `;
+
+    const params = [userId];
 
     try {
-        //retrieve all for a user code
-        return {statusCode: 200, body: 'Retrieved all the things!'};
+        const result = await client.query(query, params);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(result),
+        };
     } catch (dbError) {
         return {statusCode: 500, body: JSON.stringify(dbError)};
     }
