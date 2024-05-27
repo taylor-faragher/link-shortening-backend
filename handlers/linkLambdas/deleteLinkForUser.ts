f49eee52b2b834343321ed8a15d0534b01f4f5c8
Taylor Faragher <faragher6@gmail.com>
2024-05-27 01:05:21 -0500

    Adds logic to delete link lambda

--- handlers/linkLambdas/deleteLinkForUser.ts
+++ handlers/linkLambdas/deleteLinkForUser.ts
@@ -1,14 +1,43 @@
+import {connect} from 'ts-postgres';
 import {LinkShorteningResponse} from '../../lib/types/types';
+import {getSecretValue} from '../utils/getSecretValue';
 
 export const handler = async (event): Promise<LinkShorteningResponse> => {
-    if (!event.body) {
-        return {statusCode: 400, body: 'invalid request, you are missing the parameter body'};
+    const linkId = event.pathParameters.linkId;
+    if (!linkId) {
+        return {statusCode: 400, body: 'invalid request, you are missing path parameters'};
     }
 
-    //define params for request here
+    const {password, username, host, dbname} = await getSecretValue('LinkShortenerMasterSecret');
+
+    await using client = await connect({
+        user: username,
+        host: host,
+        database: dbname,
+        password: password,
+    });
+
+    const query = `
+        DELETE FROM links 
+        WHERE link_id = $1
+    `;
+
+    const params = [linkId];
+
     try {
-        //DELETE CODE
-        return {statusCode: 200, body: `Deleted item for user`};
+        let record = {};
+        const result = await client.query(query, params);
+        if (result.status == 'DELETE 1') {
+            record = {
+                status: 'Successfully Deleted',
+            };
+        } else {
+            return {statusCode: 400, body: 'Deletion Failed!'};
+        }
+        return {
+            statusCode: 200,
+            body: JSON.stringify(record),
+        };
     } catch (dbError) {
         return {statusCode: 500, body: JSON.stringify(dbError)};
     }
