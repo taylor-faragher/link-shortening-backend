4236e4cf4253b92f1704aab7409ebc23d73bb872
Taylor Faragher <faragher6@gmail.com>
2024-05-26 21:34:56 -0500

    Updates Random value utility to also check to make sure that value doesn't exist in the database too

--- handlers/utils/getRandomValue.ts
+++ handlers/utils/getRandomValue.ts
@@ -1,7 +1,20 @@
 const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-export const getRandomValue = () => {
-    let result = '';
-    for (let i = 8; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+export const getRandomValue = async (client): Promise<string> => {
+    const idQuery = `
+        SELECT link_id from links
+        WHERE link_id = $1
+    `;
+
+    let isValid = false;
+    let result: string = '';
+    while (!isValid) {
+        result = '';
+        for (let i = 8; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+        const checkId = await client.query(idQuery, [result]);
+        if (checkId.status == 'SELECT 0') {
+            isValid = true;
+        }
+    }
     return result;
 };
