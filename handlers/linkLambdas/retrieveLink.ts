3849a935c0c785fa79a35556fccece42e0ee8066
Taylor Faragher <faragher6@gmail.com>
2024-05-26 21:33:06 -0500

    Sets up logic for retrieving a link

--- handlers/linkLambdas/retrieveLink.ts
+++ handlers/linkLambdas/retrieveLink.ts
@@ -1,13 +1,37 @@
+import {connect} from 'ts-postgres';
 import {LinkShorteningResponse} from '../../lib/types/types';
+import {getSecretValue} from '../utils/getSecretValue';
 
 export const handler = async (event): Promise<LinkShorteningResponse> => {
-    if (!event.body) {
-        return {statusCode: 400, body: 'invalid request, you are missing the parameter body'};
+    const linkId = event.pathParameters.linkId;
+    if (!linkId) {
+        return {statusCode: 400, body: 'invalid request, you are missing the path parameter linkId'};
     }
+    const {password, username, host, dbname} = await getSecretValue('LinkShortenerMasterSecret');
+
+    await using client = await connect({
+        user: username,
+        host: host,
+        database: dbname,
+        password: password,
+    });
+
+    const query = `
+        SELECT * FROM links 
+        WHERE link_id = $1
+    `;
+
+    const params = [linkId];
 
     try {
-        //retrieve 1 link code here
-        return {statusCode: 200, body: 'Retrieved one of the things!'};
+        const result = await client.query(query, params);
+        const row = result.rows[0];
+        const record = {
+            link_id: row.get('link_id'),
+            link_url: row.get('link_url'),
+            description: row.get('description'),
+        };
+        return {statusCode: 200, body: JSON.stringify(record)};
     } catch (dbError) {
         return {statusCode: 500, body: JSON.stringify(dbError)};
     }
