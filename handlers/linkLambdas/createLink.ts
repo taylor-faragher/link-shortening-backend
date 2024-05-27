d7cdda71ef488c9b160d0507644396a075a10681
Taylor Faragher <faragher6@gmail.com>
2024-05-26 21:32:38 -0500

    Adds a bunch of logic to create link; adds checking link count; updates return record

--- handlers/linkLambdas/createLink.ts
+++ handlers/linkLambdas/createLink.ts
@@ -2,6 +2,7 @@ import {connect} from 'ts-postgres';
 import {LinkShorteningResponse} from '../../lib/types/types';
 import {getRandomValue} from '../utils/getRandomValue';
 import {getSecretValue} from '../utils/getSecretValue';
+import {countUserLinks} from '../utils/countUserLinks';
 
 export const handler = async (event): Promise<LinkShorteningResponse> => {
     if (!event.body) {
@@ -9,9 +10,13 @@ export const handler = async (event): Promise<LinkShorteningResponse> => {
     }
     const {password, username, host, dbname} = await getSecretValue('LinkShortenerMasterSecret');
 
-    const item = typeof event.body == 'object' ? event.body : JSON.parse(event.body);
-    const linkId = getRandomValue();
-
+    const link = typeof event.body == 'object' ? event.body : JSON.parse(event.body);
+    const url = link.url;
+    const description = link.description;
+    const userId = link.userId;
+    if (!userId || !description || !url) {
+        return {statusCode: 400, body: 'invalid request, you are missing items from the body'};
+    }
     await using client = await connect({
         user: username,
         host: host,
@@ -19,11 +24,39 @@ export const handler = async (event): Promise<LinkShorteningResponse> => {
         password: password,
     });
 
+    const linkCount = await countUserLinks(client, userId);
+    if (!linkCount) {
+        return {statusCode: 400, body: `We're sorry. You have created too many links.`};
+    }
+
+    const linkId = await getRandomValue(client);
+
+    const linkQuery = `
+        INSERT INTO links (link_id, link_url, description)
+        VALUES ($1, $2, $3)
+        RETURNING *
+    `;
+
+    const linkUserQuery = `
+        INSERT INTO linkuseridtolinksid (user_id, link_id) 
+        VALUES ($1, $2)
+    `;
+
+    const linkParams = [linkId, url, description];
+    const linkUserParams = [userId, linkId];
     try {
-        const result = await client.query('SELECT * FROM links');
+        const result = await client.query(linkQuery, linkParams);
+        await client.query(linkUserQuery, linkUserParams);
+        const row = result.rows[0];
+        const record = {
+            link_id: row.get('link_id'),
+            link_url: row.get('link_url'),
+            description: row.get('description'),
+            status: result.status,
+        };
         return {
             statusCode: 200,
-            body: `Created id ${linkId} for item: ${JSON.stringify(item.url)}. Also here are the results: ${JSON.stringify(result)}`,
+            body: JSON.stringify(record),
         };
     } catch (dbError) {
         const errorResponse = `database error: ${dbError}`;
