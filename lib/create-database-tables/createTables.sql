fdc57b40dfbc6a21bea3fa89699f6975af384995
Taylor Faragher <faragher6@gmail.com>
2024-05-26 19:16:45 -0500

    Updates table creation to update row names and make primary keys be custom instead of serial

--- lib/create-database-tables/createTables.sql
+++ lib/create-database-tables/createTables.sql
@@ -1,23 +1,22 @@
 CREATE TABLE IF NOT EXISTS links (
-        id SERIAL PRIMARY KEY,
-        url VARCHAR(2048) NOT NULL,
+        link_id VARCHAR(10) PRIMARY KEY,
+        link_url VARCHAR(2048) NOT NULL,
         description TEXT,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
 CREATE TABLE IF NOT EXISTS linkuser (
-        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(50) PRIMARY KEY,
         username VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL UNIQUE,
         profileInfo JSONB NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        profile_info JSONB
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
 CREATE TABLE IF NOT EXISTS linkuseridtolinksid (
-        user_id INTEGER NOT NULL,
-        link_id INTEGER NOT NULL,
+        user_id VARCHAR NOT NULL,
+        link_id VARCHAR NOT NULL,
         PRIMARY KEY (user_id, link_id),
-        FOREIGN KEY (user_id) REFERENCES "linkuser"(id) ON DELETE CASCADE,
-        FOREIGN KEY (link_id) REFERENCES "links"(id) ON DELETE CASCADE
+        FOREIGN KEY (user_id) REFERENCES "linkuser"(user_id) ON DELETE CASCADE,
+        FOREIGN KEY (link_id) REFERENCES "links"(link_id) ON DELETE CASCADE
     );
