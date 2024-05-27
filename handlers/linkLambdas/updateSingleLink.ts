533fe173082bbc1636104a3ac72f2aeba2bc4b42
Taylor Faragher <faragher6@gmail.com>
2024-05-27 12:27:33 -0500

    Adds logic to the update link lambda

--- handlers/linkLambdas/updateSingleLink.ts
+++ handlers/linkLambdas/updateSingleLink.ts
@@ -1,27 +1,48 @@
+import {connect} from 'ts-postgres';
 import {LinkShorteningResponse} from '../../lib/types/types';
+import {getSecretValue} from '../utils/getSecretValue';
 
 export const handler = async (event): Promise<LinkShorteningResponse> => {
     if (!event.body) {
         return {statusCode: 400, body: 'invalid request, you are missing the parameter body'};
     }
 
-    const editedItemId = event.pathParameters.id;
-    if (!editedItemId) {
-        return {statusCode: 400, body: 'invalid request, you are missing the path parameter id'};
+    const link = typeof event.body == 'object' ? event.body : JSON.parse(event.body);
+    const url = link.url;
+    const description = link.description;
+    const userId = link.userId;
+    if (!userId || !description || !url) {
+        return {statusCode: 400, body: 'invalid request, you are missing items from the body'};
     }
 
-    //check if it is an object and parse if it is
-    const editedItem = typeof event.body == 'object' ? event.body : JSON.parse(event.body);
-
-    const editedItemProperties = Object.keys(editedItem);
-    if (!editedItem || editedItemProperties.length < 1) {
-        return {statusCode: 400, body: 'invalid request, no arguments provided'};
+    const editedLinkId = event.pathParameters.linkId;
+    if (!editedLinkId) {
+        return {statusCode: 400, body: 'invalid request, you are missing a path parameter'};
     }
+
+    const {password, username, host, dbname} = await getSecretValue('LinkShortenerMasterSecret');
+
+    await using client = await connect({
+        user: username,
+        host: host,
+        database: dbname,
+        password: password,
+    });
+
+    const query = `
+        UPDATE links SET link_url = $1, description = $2
+        WHERE link_id = $3
+    `;
+
+    const params = [url, description, editedLinkId];
 
     try {
-        //CODE TO UPDATE!
-        return {statusCode: 204, body: `Edited item to now be ${editedItem}`};
+        await client.query(query, params);
+        return {
+            statusCode: 204,
+            body: 'Updated Successfully!',
+        };
     } catch (dbError) {
-        return {statusCode: 500, body: 'Failed to update. sorry'};
+        return {statusCode: 500, body: `Failed to update. Error: ${dbError}`};
     }
 };
