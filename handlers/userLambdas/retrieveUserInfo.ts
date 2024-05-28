63a94a16ef69c13d1c97ba6fd999f3557baa86b2
Taylor Faragher <faragher6@gmail.com>
2024-05-27 20:58:08 -0500

    Updates logic for retrieve user info

--- handlers/userLambdas/retrieveUserInfo.ts
+++ handlers/userLambdas/retrieveUserInfo.ts
@@ -1,14 +1,16 @@
 import {connect} from 'ts-postgres';
 import {LinkShorteningResponse} from '../../lib/types/types';
 import {getSecretValue} from '../utils/getSecretValue';
+import {mapUserInfo} from '../utils/mapUserInfo';
 
 export const handler = async (event): Promise<LinkShorteningResponse> => {
-    if (!event.body) {
-        return {statusCode: 400, body: 'invalid request, you are missing the parameter body'};
+    if (!event.pathParameters.userId) {
+        return {statusCode: 400, body: 'invalid request, you are missing a path parameter'};
     }
-    const {password, username, host, dbname} = await getSecretValue('LinkShortenerMasterSecret');
 
-    const item = typeof event.body == 'object' ? event.body : JSON.parse(event.body);
+    const userId = event.pathParameters.userId;
+
+    const {password, username, host, dbname} = await getSecretValue('LinkShortenerMasterSecret');
 
     await using client = await connect({
         user: username,
@@ -17,12 +19,20 @@ export const handler = async (event): Promise<LinkShorteningResponse> => {
         password: password,
     });
 
+    const query = `
+        SELECT * FROM linkuser
+        WHERE user_id = $1;
+    `;
+
+    const params = [userId];
+
     try {
-        const result = await client.query('SELECT * FROM links');
-        console.log(`items: ${item} \n results: ${JSON.stringify(result)}`);
+        const result = await client.query(query, params);
+        const record = mapUserInfo(result.rows);
+
         return {
             statusCode: 200,
-            body: `User Created`,
+            body: JSON.stringify(record),
         };
     } catch (dbError) {
         const errorResponse = `database error: ${dbError}`;
