5421ac6eeb8107715e6791953026521ffbfae2ad
Taylor Faragher <faragher6@gmail.com>
2024-05-27 22:56:04 -0500

    Adds logic for update user profile

--- handlers/userLambdas/updateUserProfile.ts
+++ handlers/userLambdas/updateUserProfile.ts
@@ -6,9 +6,14 @@ export const handler = async (event): Promise<LinkShorteningResponse> => {
     if (!event.body) {
         return {statusCode: 400, body: 'invalid request, you are missing the parameter body'};
     }
+
+    if (!event.pathParameters.userId) {
+        return {statusCode: 400, body: 'invalid request, you are missing path parameters'};
+    }
+    const userId = event.pathParameters.userId;
     const {password, username, host, dbname} = await getSecretValue('LinkShortenerMasterSecret');
 
-    const item = typeof event.body == 'object' ? event.body : JSON.parse(event.body);
+    const profileInfo = typeof event.body == 'object' ? event.body : JSON.parse(event.body);
 
     await using client = await connect({
         user: username,
@@ -17,15 +22,30 @@ export const handler = async (event): Promise<LinkShorteningResponse> => {
         password: password,
     });
 
+    const query = `
+        UPDATE linkuser
+        SET profileInfo = $1
+        WHERE user_id = $2;
+    `;
+
+    const params = [profileInfo, userId];
     try {
-        const result = await client.query('SELECT * FROM links');
-        console.log(`items: ${item} \n results: ${JSON.stringify(result)}`);
+        await client.query(query, params);
         return {
             statusCode: 200,
-            body: `User Created`,
+            body: 'Profile Updated!',
         };
     } catch (dbError) {
         const errorResponse = `database error: ${dbError}`;
         return {statusCode: 500, body: errorResponse};
     }
 };
+// Leaving these here in case I, or someone else, wants to use them
+
+// Query for setting specific field
+// UPDATE linkuser
+// SET profile = jsonb_set(profile, '{age}', '32')
+// WHERE user_id = 1;
+
+// -- Example line below to update nested value
+// -- SET profile = jsonb_set(profile, '{preferences,theme}', '"dark"')
