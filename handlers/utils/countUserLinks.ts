9d7c2ef05e922d5707e4ad03c767a3cd20a4f126
Taylor Faragher <faragher6@gmail.com>
2024-05-28 00:03:59 -0500

    Removes if check from counting method; updates return to include countNumber

--- handlers/utils/countUserLinks.ts
+++ handlers/utils/countUserLinks.ts
@@ -1,4 +1,4 @@
-export const countUserLinks = async (client, userId): Promise<boolean> => {
+export const countUserLinks = async (client, userId) => {
     const query = `
         SELECT user_id, COUNT(*) AS link_count
         FROM linkuseridtolinksid
@@ -6,19 +6,17 @@ export const countUserLinks = async (client, userId): Promise<boolean> => {
         GROUP BY user_id;
     `;
 
-    let count = '0';
     const result = await client.query(query, [userId]);
     // this is done because the response has a BigInt in it so it needs to be handled
     // https://github.com/GoogleChromeLabs/jsbi/issues/30
-    if (result.row > 0) {
-        const linkCount = JSON.parse(
-            JSON.stringify(
-                result,
-                (_key, value) => (typeof value === 'bigint' ? value.toString() : value) // return everything else unchanged
-            )
-        );
-        count = linkCount.rows[0][1];
-    }
+    const linkCount = JSON.parse(
+        JSON.stringify(
+            result,
+            (_key, value) => (typeof value === 'bigint' ? value.toString() : value) // return everything else unchanged
+        )
+    );
+    const count = linkCount.rows[0][1];
     const countNumber: number = +count;
-    return countNumber >= 10;
+    const valid = countNumber >= 10;
+    return {valid, countNumber};
 };
